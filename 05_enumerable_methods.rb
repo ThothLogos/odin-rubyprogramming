8676bda7603324333f1e447dcdb2370f9592c368
ThothLogos <naughton.joshua@gmail.com>
2015-01-23 01:12:25 -0500

    Initial framework

---
+++ 05_enumerable_methods.rb
@@ -0,0 +1,53 @@
+# The Odin Project - Section 3: Ruby Programming
+# Project Building Blocks, No. 5 Enumerable Methods
+
+# 
+
+module Enumerable
+
+   
+   def my_each
+      
+      
+      
+
+
+   end
+
+
+
+
+
+   
+   def my_each_with_index
+   end
+
+
+   def my_select
+   end
+
+
+   def my_all?
+   end
+
+
+   def my_any?
+   end
+
+
+   def my_none?
+   end
+
+   def my_count
+   end
+
+   def my_map
+   end
+
+   def my_inject
+   end
+
+   def multiply_elements
+   end
+
+end
