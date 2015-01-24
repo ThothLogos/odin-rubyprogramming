ff41d309bee881a3fbceb1990f81e516cf77a83d
ThothLogos <naughton.joshua@gmail.com>
2015-01-24 01:04:28 -0500

    Add my_any?, my_none?, my_count, comments

--- 05_enumerable_methods.rb
+++ 05_enumerable_methods.rb
@@ -3,7 +3,8 @@
 
 module Enumerable
    
-
+   # Iterates through each element inside of a collection and forwards each
+   # element's value to an optional block
    def my_each
 
       if block_given?
@@ -13,51 +14,108 @@ module Enumerable
          return self.to_enum; end # If no block, return enum
    end
 
-
+   # Forwards each element's index, rather than value, to the block
    def my_each_index
 
       if block_given?
          for i in self
-            yield(self.index(i)); end # Send the index instead of the value
+            yield(self.index(i)); end
       else
          return self.to_enum; end
    end
 
 
+   # Returns new array containing all elements that return true from the block
    def my_select
 
       result = []
 
       if block_given?
          for i in self
-            if yield(i) == true      # Anything returning as true
+            if yield(i) == true      # Anything returning true from the block
                result << i; end; end # is placed in a new array
-         return result               # and returned to the user
+         return result
       else
          return self.to_enum; end 
    end
 
 
+   # Returns true if the block never returns false or nil. If no block is given
+   # an implicit block is added which will return true. The implicit block checks
+   # to see if each element is an object, which it will always be
    def my_all?(&block)
 
-      truthiness = true
+      truthiness = true # Assume all will match
 
       if !block_given?
-         block ||= Proc.new { |obj| obj }; end
-
+         block ||= Proc.new { |obj| obj }; end # Implicit block
       for i in self
+         # One bad apple spoils the bunch, return false unless everything is true
          truthiness = false if block.call(i) == false || block.call(i) == nil; end
-
       return truthiness
    end
 
 
-   def my_any?
+   # Returns true if the block ever returns anything besides false or nil. As with
+   # my_all?, the implicit block is added when non is provided, which will result
+   # in a true return
+   def my_any?(&block)
 
-      
-      
+      truthiness = false # Assume nothing will match
+
+      if !block_given?
+         block ||= Proc.new { |obj| obj }; end
+      for i in self
+         # If any single element is valid, return true
+         truthiness = true if block.call(i) != false && block.call(i) != nil; end
+      return truthiness
    end
 
+
+   # Returns false if the block ever returns a single true. If no block is given, it checks
+   # each element of the calling collection instead and, like with a block, returns false 
+   # if a true is ever found
+   def my_none?
+
+      truthiness = true # Assume we won't hit any matches
+      
+      for i in self
+         if block_given? # If we have a block, yield
+            truthiness = false if yield(i) == true
+         else # If no block, check the caller's elements instead
+            truthiness = false if i == true; end; end
+      return truthiness
+   end
+
+
+   # With no arguments passed, returns the number of items in the collection. If an
+   # argument is included, it will count the instances in the collection equal to the
+   # item passed in. If a block is provided, it will count the number of true returns
+   # from the block
+   def my_count(item = nil)
+
+      count = 0
+
+      for i in self
+         if block_given? # Count trues inside the block
+            count += 1 if yield(i) == true
+         elsif item != nil # Count items matching input
+            count += 1 if i == item       
+         else # No block, no arg, count self
+            count += 1; end; end
+      return count
+   end
+
+
+   # Returns a new array with the results of running a block once for every element
+   # inside the calling collection. If no block is provided, an enum is returned
+   def my_map
+
+      if block_given?
+
+      else
+         return self.to_enum; end
+   end
 
 end
 
@@ -76,10 +134,14 @@ array = [6,3,4,11]
 
 words = ["ant", "bear", "cat"]
 
-words.all? { |word| word.length >= 3 }
-words.all? { |word| word.length >= 4 }
+#words.my_all? { |word| word.length >= 3 }
+#words.my_all? { |word| word.length >= 4 }
+#words.my_all?
 
-words.my_all? { |word| word.length >= 3 }
-words.my_all? { |word| word.length >= 4 }
+#words.my_any? { |word| word.length >= 3 }
+#words.my_any? { |word| word.length >= 5 }
+#[nil, true, 99].my_any?
 
-words.my_all?
+#array.my_count
+#array.my_count(3)
+#array.my_count { |x|  x % 2 == 0 }
