b12390e7f294def9f81d61d3d9fc6fdefced2054
ThothLogos <naughton.joshua@gmail.com>
2015-01-28 07:37:58 -0500

    Add hit checking/storage, begin hit logic

--- mastermind/gamedata.rb
+++ mastermind/gamedata.rb
@@ -8,16 +8,62 @@ class GameData
 
     for i in 1..12
       @attempts[i] = [" ", " ", " ", " "]
-      @hits[i] = "       "
+      @attempts[i] = ["_", "_", "_", "_"] if i == 12
+      @hits[i] = [" ", " ", " ", " "]
     end
 
   end
 
+  def duplicate?(attempt)
+    @attempts.each do |entry|
+      return true if entry == attempt
+    end
+    return false
+  end
    
 
   # Store history of code-breaker guesses, integers as an index storying 4-integer arrays
-  def store_attempt(turn, code)
-    @attempts[turn] = code 
+  def store_attempt(turn, attempt)
+    @attempts[turn] = attempt 
+  end
+
+  def check_hits(code, attempt)
+
+    hits = [" ", " ", " ", " "]
+    
+    return ["!", "!", "!", "!"] if code == attempt
+     
+    bang = 0  
+    for i in 0..3
+      bang += 1 if code[i] == attempt[i]; end
+
+    star = 0
+    for i in 0..3
+      for j in 0..3
+        if code[i] == attempt[j]
+          star += 1 ; end
+    end
+    star -= bang
+    
+    i = 0
+    if bang > 0
+      for k in 0..bang
+        hits[i] = "!"
+        i += 1; end
+    end
+
+    if star > 0
+      for p in 0..star
+        hits[i] = "*"
+        i += 1; end
+    end
+
+    return hits
+
+  end
+
+  def store_hits(turn, hits)
+    @hits[turn] = hits
   end
 
   def attempts
