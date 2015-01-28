b4f16df99940665bab30c296f2f8ed750e29c2a5
ThothLogos <naughton.joshua@gmail.com>
2015-01-28 07:36:33 -0500

    To string

--- mastermind/mastermindAI.rb
+++ mastermind/mastermindAI.rb
@@ -9,9 +9,8 @@ class MastermindAI
     def generate_code
       code = []
       4.times do
-          code << 1 + rand(6)
+          code << (1 + rand(6)).to_s
       end
       return code
     end
-
 end
