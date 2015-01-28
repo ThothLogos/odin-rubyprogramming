36677c6519094589c4b9fa8223919405c700581a
ThothLogos <naughton.joshua@gmail.com>
2015-01-28 16:24:59 -0500

    Completed check_hits, debug messages

--- mastermind/gamedata.rb
+++ mastermind/gamedata.rb
@@ -14,9 +14,10 @@ class GameData
 
   end
 
-  def duplicate?(attempt)
-    @attempts.each do |entry|
-      return true if entry == attempt
+  def duplicate?(break_attempt)
+
+    for i in 1..12
+      return true if @attempts[i] == break_attempt
     end
     return false
   end
@@ -32,7 +33,12 @@ class GameData
     hits = [" ", " ", " ", " "]
     
     return ["!", "!", "!", "!"] if code == attempt
-     
+    
+    instances_attempt = 0
+    instances_code = 0
+    max_instances = 0
+    credited = []
+
     bang = 0  
     for i in 0..3
       bang += 1 if code[i] == attempt[i]; end
@@ -40,23 +46,43 @@ class GameData
     star = 0
     for i in 0..3
       for j in 0..3
-        if code[i] == attempt[j]
-          star += 1 ; end
+        if attempt[i] == code[j]
+          # Maximum number of hits possible constrained by actual duplicates
+          instances_attempt = attempt.count(attempt[i])
+          instances_code = code.count(attempt[i])
+          instances_attempt > instances_code ? max_instances = instances_code : max_instances = instances_attempt
+          if credited.include?(attempt[i])
+            puts "already credited #{i}, #{j}: #{code[j]}"
+          else
+            max_instances.times do
+              star +=1; end
+            credited << attempt[i]; end
+        end
+      end
     end
+    puts "Bangs found: #{bang}"
+    puts "Stars found: #{star}"
     star -= bang
+    puts "Stars mod: #{star}"
+    gets
     
     i = 0
     if bang > 0
-      for k in 0..bang
+      for k in 1..bang
         hits[i] = "!"
         i += 1; end
     end
 
+    puts "Hits so far: #{hits}"
+
     if star > 0
-      for p in 0..star
+      for p in 1..star
         hits[i] = "*"
         i += 1; end
     end
+
+    puts "Hits so far: #{hits}"
+    gets
 
     return hits
 
