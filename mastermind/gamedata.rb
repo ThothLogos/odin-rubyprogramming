aa973c4788a1b372bf10db44fc5a0907fe486151
ThothLogos <naughton.joshua@gmail.com>
2015-01-28 06:15:41 -0500

    Getters

--- mastermind/gamedata.rb
+++ mastermind/gamedata.rb
@@ -2,20 +2,31 @@ class GameData
 
 
   def initialize
+
     @attempts = Hash.new
+    @hits = Hash.new
+
+    for i in 1..12
+      @attempts[i] = [" ", " ", " ", " "]
+      @hits[i] = "       "
+    end
+
   end
 
    
 
   # Store history of code-breaker guesses, integers as an index storying 4-integer arrays
-  def store_attempt(code)
-    @attempts[@attempts.length + 1] = code 
+  def store_attempt(turn, code)
+    @attempts[turn] = code 
   end
 
-  def getattempts
+  def attempts
     return @attempts
   end
 
+  def hits
+    return @hits
+  end
 
 
 end
