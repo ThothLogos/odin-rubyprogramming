cbc2501b26b6d5f861d5af513cd775a65df83be0
ThothLogos <naughton.joshua@gmail.com>
2015-01-29 07:56:11 -0500

    Code cleanup

--- mastermind/gamedata.rb
+++ mastermind/gamedata.rb
@@ -3,41 +3,27 @@
 
 class GameData
 
-
   def initialize
-
     @attempts = Hash.new
     @hits = Hash.new
-
     for i in 1..12
       @attempts[i] = [" ", " ", " ", " "]
       @attempts[i] = ["_", "_", "_", "_"] if i == 12
       @hits[i] = [" ", " ", " ", " "]
     end
-
   end
 
   def duplicate?(break_attempt)
-
     for i in 1..12
       return true if @attempts[i] == break_attempt
     end
     return false
   end
    
-
-  # Store history of code-breaker guesses, integers as an index storying 4-integer arrays
-  def store_attempt(turn, attempt)
-    @attempts[turn] = attempt 
-  end
-
   def check_hits(code, attempt)
-
     hits = [" ", " ", " ", " "]
-    
     # Check for exact match right away
     return ["!", "!", "!", "!"] if code == attempt
-    
     # Temp array which will hold numbers we've already counted, to ensure
     # hits are exclusive
     credited = []
@@ -45,8 +31,8 @@ class GameData
     # Count up the exact matches of a number being in the correct spot, represented by !
     bang = 0  
     for i in 0..3
-      bang += 1 if code[i] == attempt[i]; end
-
+      bang += 1 if code[i] == attempt[i]
+    end
     # After the bangs, count the stars - this one is a bit trickier to implement
     star = 0
     for i in 0..3
@@ -65,32 +51,23 @@ class GameData
         end
       end
     end
-
     star -= bang
-    
     i = 0
     if bang > 0
       for k in 1..bang
         hits[i] = "!"
         i += 1; end
-    end
-
-    if star > 0
+    elsif star > 0
       for p in 1..star
         hits[i] = "*"
         i += 1; end
     end
-
     return hits
-
   end
 
-  def get_bangs(turn, data)
-
-  end
-
-  def get_stars(turn, data)
-    
+  # Store history of code-breaker guesses, integers as an index storying 4-integer arrays
+  def store_attempt(turn, attempt)
+    @attempts[turn] = attempt 
   end
 
   def store_hits(turn, hits)
@@ -104,6 +81,5 @@ class GameData
   def hits
     return @hits
   end
-
 
 end
