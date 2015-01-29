e17c40005e16f8c5c357735329286a79f01b33de
ThothLogos <naughton.joshua@gmail.com>
2015-01-29 07:55:16 -0500

    Refactor DYR

--- mastermind/game.rb
+++ mastermind/game.rb
@@ -8,110 +8,97 @@ require_relative 'gamedata.rb'
 class Game
 
   def initialize
+    # These two flags are used to control animations not repeating upon every "play again"
+    @mode_one_first_run = true
+    @mode_two_first_run = true
     @ai = MastermindAI.new
+    # All animations and graphics are in view
     @view = View.new
+    # Box slide intro
     @view.intro_splash_animate
-    gets
+    gets # Wait for enter
     main_menu
   end
 
   def main_menu
-    @new_game = true
+    @new_game = true # Resets setup flags for each game mode
     @continue = false
-    @view.main_menu_animate
+    @view.main_menu_animate # Book animation
     menu_choice = gets.chomp
-
     case menu_choice
       when "1"
+        mode_one
         @game_mode = 1
-        mode_one_loop
       when "2"
+        mode_two
         @game_mode = 2
-        mode_two_loop
       when "3"
         @view.how_to_play
+        main_menu
       when "4"
         @view.about
-      when "X", "x", "Q", "q"
+        main_menu
+      when "X", "x", "Q", "q", "quit", "Quit", "exit", "Exit"
         abort("Thanks for playing!")
       else
-        main_menu
+        main_menu # Reload menu if invalid input
     end
   end
 
-  def mode_one_loop
+  def mode_one
 
+    # Only runs on first call to this mode
     if @new_game == true
       @turn = 1  
       @continue = true
       @new_game = false
       @data = GameData.new
-      @code = @ai.generate_code
-      @view.game_state_animation 
+      @code = @ai.generate_code # CPU makes random code to solve
+      @view.game_state_animation # Scrambled numbers animation
     end
 
-    while @continue == true
+    # Mode one's primary game loop
+    while @continue
 
-      valid = false
-      until valid 
-        @view.game_state(@data.attempts, @data.hits, @turn, @code) 
-        print " Enter your potential solution to break the code: "
-        input = gets.chomp
-        
-        next if !is_good?(input)
+      @view.game_state(@data.attempts, @data.hits, @turn) 
+      print " Enter your potential solution to break the code: "
+      input = gets.chomp
+      
+      # Check input, restart loop if it's bad
+      mode_one if !is_good?(input)
+      # Small moment of user feedback
+      @view.game_state(@data.attempts, @data.hits, @turn, @code, "  Attempting Code Break  ")
+      sleep 0.8 # User experience delay
 
-        @view.game_state(@data.attempts, @data.hits, @turn, @code, "  Attempting Code Break  ")
+      break_attempt = []
+      input.each_char { |c| break_attempt << c }
+
+      # Ask the GameData object to check the record, refuse duplicate break attempts
+      if @data.duplicate?(break_attempt) == true
+        puts " You've used this exact combination before, try something else."
         sleep 1
-
-        break_attempt = []
-        input.each_char do |c|
-          break_attempt << c; end
-
-        if @data.duplicate?(break_attempt) == true
-          puts " You've used this exact combination before, try something else."
-          sleep 1
-          mode_one_loop
-        else
-          valid = true
-        end
-
+        mode_one
       end
 
       @data.store_attempt(@turn, break_attempt)
       hits = @data.check_hits(@code, break_attempt)
       @data.store_hits(@turn, hits)
-
-      if hits == ["!","!","!","!"]
-        @continue = false
-        @view.game_win
-      elsif @turn >= 12
-        @continue = false
-        @view.game_loss
-      end
-      if !@continue
-        input = gets.chomp
-        if input == "y" || input == "Y" || input == "yes" || input == "Yes" || input == "YES"
-          @new_game = true
-          main_menu
-        else
-          abort("Thanks for playing!"); end
-      end
-
-          
+      
+      check_win(hits, game_mode)
       @turn += 1
-
     end
   end
 
 
-  def mode_two_loop
+  def mode_two
 
     if @new_game == true
       @turn = 1  
       @continue = true
       @newgame = false
       @data = GameData.new
-      @view.challenge
+      @view.challenge if @mode_two_first_run
+      @mode_two_first_run = false
     end
 
     valid = false
@@ -133,7 +120,7 @@ class Game
     while @continue
 
       @view.game_state(@data.attempts, @data.hits, @turn, @code, "     Challenge  Mode     ")
-      sleep 1
+      sleep 0.6
       @view.game_state(@data.attempts, @data.hits, @turn, @code, "   Evaluating Options    ")      
       
       break_attempt = @ai.generate_code
@@ -145,15 +132,21 @@ class Game
       @data.store_attempt(@turn, break_attempt)
       hits = @data.check_hits(@code, break_attempt)
       @data.store_hits(@turn, hits)
+      
+      sleep 1.2
+      check_win(hits, game_mode)
+      @turn += 1
+    end
+  end
 
-      sleep 2
-
+  def check_win(hits, game_mode)
+      
       if hits == ["!","!","!","!"]
         @continue = false
-        @view.game_loss
+        game_mode == 1 ? @view.game_win : @view.game_loss
       elsif @turn >= 12
         @continue = false
-        @view.game_win
+        game_mode == 1 ? @view.game_loss : @view.game_loss
       end
       if !@continue
         input = gets.chomp
@@ -163,10 +156,7 @@ class Game
         else
           abort("Thanks for playing!"); end
       end
-      @turn += 1
-    end
   end
-
 
 
   def is_good?(input)
