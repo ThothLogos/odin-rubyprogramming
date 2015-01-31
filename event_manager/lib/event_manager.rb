1b852e84b877d8891d8bf1867e6321f89353f710
ThothLogos <naughton.joshua@gmail.com>
2015-01-31 18:28:06 -0500

    Column parsing

--- event_manager/lib/event_manager.rb
+++ event_manager/lib/event_manager.rb
@@ -1,3 +1,9 @@
 # The Odin Porject - Section 3: Ruby Programming
 # Project File I/O and Serialization, No. 1 Event Manager
 
+
+lines = File.readlines "../event_attendees.csv"
+lines.each do |line|
+  columns = line.split(",")
+  puts columns[2]
+end
