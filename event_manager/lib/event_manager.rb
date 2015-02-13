692148dc705e7ff099c4473cc862006cbcd8f34f
ThothLogos <naughton.joshua@gmail.com>
2015-02-13 00:46:39 -0500

    Add zipcode santization

--- event_manager/lib/event_manager.rb
+++ event_manager/lib/event_manager.rb
@@ -1,9 +1,32 @@
 # The Odin Porject - Section 3: Ruby Programming
 # Project File I/O and Serialization, No. 1 Event Manager
 
+require "csv"
 
-lines = File.readlines "../event_attendees.csv"
-lines.each do |line|
-  columns = line.split(",")
-  puts columns[2]
+def sanitize_zipcode(zipcode)
+  if zipcode.nil?
+    print "No code, default."
+    zipcode = "00000"
+  elsif zipcode.length > 5
+    print "Long code, cutting... "
+    zipcode = zipcode[0..4]
+  elsif zipcode.length < 5
+    print "Short code, padding... "
+    zipcode = zipcode.rjust(5, "0")
+  else
+    zipcode
+  end  
 end
+
+
+
+
+contents = CSV.open("../event_attendees.csv", headers: true, header_converters: :symbol)
+contents.each do |row|
+  name = row[:first_name]
+  zipcode = sanitize_zipcode(row[:zipcode])
+  print " " + name + " " + zipcode + "\n"
+end
+
+
+
