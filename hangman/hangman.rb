e2bc37fe7b902edeab900cda72eba04e19bc9a4c
ThothLogos <naughton.joshua@gmail.com>
2015-02-13 05:43:21 -0500

    Add intro_splash and main_menu fade-ins for text, basic input checking for menu

--- hangman/hangman.rb
+++ hangman/hangman.rb
@@ -18,8 +18,23 @@ class Game
 
   def main_menu
     
-    @view.show_main_menu
+    @view.show_main_menu_animate
 
+    valid = false
+    until valid
+      @view.show_main_menu
+      print " Please select a menu option: "
+      input = gets.chomp
+      if input.to_i.is_a? Integer
+        input = input.to_i
+        if input >= 1 && input <= 3
+          valid = true; end
+      else
+        sleep 0.2
+        puts " Invalid entry, please try again."
+        sleep 0.5
+      end
+    end
   end
 
 end
--- hangman/view.rb
+++ hangman/view.rb
@@ -3,13 +3,11 @@
 
 
 class View
+  
 
-  
-  
   def show_intro_splash
-
-    logo = "2015_ThothLogos"
-
+    logo_i = "_______________"
+    logo_f = "2015_ThothLogos"
     3.times do
       intro_splash4
       sleep 0.1
@@ -20,19 +18,66 @@ class View
       intro_splash1
       sleep 0.1
     end
-
     message = "Press Enter to begin..."
-    intro_splash_final(message, logo)
+
+    for i in 0...logo_i.length
+      logo_i[i] = logo_f[i]
+      sleep 0.05
+      intro_splash_final(message, logo_i)
+    end
+    
+  end
+
+
+  def show_main_menu_animate
+
+    logo = "2015_ThothLogos"
+
+    one_i = "           "
+    two_i = "                  "
+    three_i = "              "
+
+
+    one_f = "1) New Game"
+    two_f = "2) Load Saved Game"
+    three_f = "3) How to Play"
+
+    one_remaining = []
+    two_remaining = []
+    three_remaining = []
+
+    for i in 0...one_i.length do one_remaining << i end
+    for i in 0...two_i.length do two_remaining << i end
+    for i in 0...three_i.length do three_remaining << i end
+
+    sizes = [one_i.length, two_i.length, three_i.length]
+    max_cycles = sizes.max
+
+    for i in 0...max_cycles
+      if i < one_i.length
+        selected = one_remaining.slice!(one_remaining.index(one_remaining.sample))
+        one_i[selected] = one_f[selected]; end
+      if i < two_i.length
+        selected = two_remaining.slice!(two_remaining.index(two_remaining.sample))
+        two_i[selected] = two_f[selected]; end
+      if i < three_i.length
+        selected = three_remaining.slice!(three_remaining.index(three_remaining.sample))
+        three_i[selected] = three_f[selected]; end
+      
+      main_menu(one_i, two_i, three_i, logo)
+      sleep 0.03
+    end    
   end
 
 
   def show_main_menu
-
     logo = "2015_ThothLogos"
+    
     one = "1) New Game"
     two = "2) Load Saved Game"
+    three = "3) How to Play"
 
-    main_menu(one, two, logo)
+    main_menu(one, two, three, logo)
   end
 
   def show_how_to_play
@@ -174,7 +219,7 @@ class View
     puts "|___________________________________________________________#{logo}___|"
   end
 
-  def main_menu(option1, option2, logo)
+  def main_menu(option1, option2, option3, logo)
     puts "\e[H\e[2J"
     puts " _____________________________________________________________________________"
     puts "|                                                                             |"
@@ -195,7 +240,7 @@ class View
     puts "|                                                                             |"
     puts "|                 #{option1}                 #{option2}              |"
     puts "|                                                                             |"
-    puts "|                                                                             |"
+    puts "|                              #{option3}                                 |"
     puts "|                                                                             |"
     puts "|                                                                             |"
     puts "|___________________________________________________________#{logo}___|"
