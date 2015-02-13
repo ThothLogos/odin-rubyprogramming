594569e2c03ab985ff916662230de18ce4d46b85
ThothLogos <naughton.joshua@gmail.com>
2015-02-13 01:11:09 -0500

    Add personalized letter template

--- event_manager/lib/event_manager.rb
+++ event_manager/lib/event_manager.rb
@@ -4,21 +4,33 @@
 require "csv"
 require "sunlight/congress"
 
+
 Sunlight::Congress.api_key = "e179a6973728c4dd3fb1204283aaccb5"
 
 def sanitize_zipcode(zipcode)
   zipcode.to_s.rjust(5,"0")[0..4]
 end
 
+def legislators_by_zipcode(zipcode)
+  legislators = Sunlight::Congress::Legislator.by_zipcode(zipcode)
+  legi_names = legislators.collect do |legi|
+    "#{legi.first_name} #{legi.last_name}"
+  end
+  legi_names = legi_names.join(", ")
+end
+
+
 contents = CSV.open("../event_attendees.csv", headers: true, header_converters: :symbol)
+template_letter = File.read "../form_letter.html"
 
 
 contents.each do |row|
   name = row[:first_name]
   zipcode = sanitize_zipcode(row[:zipcode])
-  legislators = Sunlight::Congress::Legislator.by_zipcode(zipcode)
-  legi_names = legislators.collect do |legi|
-    "#{legi.first_name} #{legi.last_name}"
-  end
-  print " " + name + " " + zipcode + " " + legi_names.to_s + "\n"
+  legislators = legislators_by_zipcode(zipcode)
+
+  personal_letter = template_letter.gsub("FIRST_NAME", name)
+  personal_letter.gsub!("LEGISLATORS", legislators)
+
+  puts personal_letter
 end
