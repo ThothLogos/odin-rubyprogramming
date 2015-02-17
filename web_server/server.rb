473842fdb2ef6d3fcba8e7b605edd46a224df61c
ThothLogos <naughton.joshua@gmail.com>
2015-02-17 18:28:09 -0500

    Add 400 Bad Request, add Content-Length and Date

--- web_server/server.rb
+++ web_server/server.rb
@@ -8,30 +8,36 @@ loop {
 
   request = client.gets
   resource = request[/\w+.html/]
-  type = request[/(GET|POST)/]
+  type = request[/(GET|POST|HEAD)/]
   version = request[/HTTP\/\d\.\d/]
   message = ""
   status = ""
-  response = ""
+  body = ""
 
   puts "Request: " + request.chomp
 
   if type == "GET"
     if File.exists?("#{resource}")
-      puts "Resource found, sending: #{resource}"
       file = File.open(resource, "r")
       file.each do |line|
-        response << line; end
+        body << line; end
       status = "200"
       message = "OK"
     else
-      puts "Resource not found, sending code 404."
       status = "404"
       message = "Not Found"
     end
-    client.puts(version + " " + status + " " + message)
-    client.puts(response)
+  elsif type == "POST"
+    #POST
+  else
+    status = "400"
+    message = "Bad Request"
   end
+
+  client.puts(version + " " + status + " " + message)
+  client.puts("Date: " + Time.now.ctime)
+  client.puts("Content-Length: " + body.length.to_s)
+  client.puts(body)
 
   client.close
 }
