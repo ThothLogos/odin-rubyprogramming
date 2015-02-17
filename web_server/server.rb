13e37a42fbdd123489fbf6d9c513e544d548f7db
ThothLogos <naughton.joshua@gmail.com>
2015-02-17 06:51:44 -0500

    Update server to parse request types, identify the resource request

--- web_server/server.rb
+++ web_server/server.rb
@@ -6,12 +6,23 @@ server = TCPServer.open(7680)
 loop {
   client = server.accept
 
+  request = client.gets
+  resource = request[/\w+.html/]
+  type = request[/(GET|POST)/]
 
-  incoming = client.gets
-  if incoming.match(/GET/)
-    puts "Request:" + incoming
-    client.puts "Request heard."
+  response = ""
+  file = File.open(resource, "r")
+  file.each do |line|
+    response << line
   end
+
+  if type == "GET"
+    puts "Request: " + request + " Sending: #{resource}"
+    client.puts(response)
+  end
+
+
+
 
   client.puts(Time.now.ctime)
   client.puts "Closing connection. "
