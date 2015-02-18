6b9ef19cc74f54daee264e28cbc27295742b49ac
ThothLogos <naughton.joshua@gmail.com>
2015-02-18 10:27:13 -0500

    Begin content-type checking

--- web_server/server.rb
+++ web_server/server.rb
@@ -1,35 +1,45 @@
 require 'socket'
 require 'json'
 
+content_type = { html: 'text/html',
+                 txt: 'text/plain',
+                 png: 'image/png',
+                 jpg: 'image/jpeg' }
 
 server = TCPServer.open(7680)
 
-loop {
-  client = server.accept
+loop do
 
-  request = client.gets
+  socket = server.accept
+
+  request = socket.gets
   resource = request[/\w+.html/]
-  type = request[/(GET|POST|HEAD)/]
+  method = request[/(GET|POST|HEAD)/]
   version = request[/HTTP\/\d\.\d/]
-  incoming = request[/Content-Length: (\d+)/]
+  incoming = request[/Content-Length: (\d*)/]
   message = ""
   status = ""
   body = ""
 
-  puts "Request: " + request.chomp
+  puts "Request: " + request
 
-  if type == "GET"
+  if method == "GET"
     if File.exists?("#{resource}")
-      file = File.open(resource, "r")
-      file.each do |line|
-        body << line; end
-      status = "200"
-      message = "OK"
+      ext = File.extname(resource).split(".").last
+      if ext == "html" || ext == "htm" || ext == "txt"
+        file = File.open(resource, "r")
+        file.each do |line|
+          body << line; end
+        status = "200"
+        message = "OK"
+      end
     else
       status = "404"
       message = "Not Found"
+      socket.puts(version + " " + status + " " + message)
+      socket.puts("Date: " + Time.now.ctime)
     end
-  elsif type == "POST"
+  elsif method == "POST"
     if File.exists?("#{resource}")
       file = File.open(resource, "r")
       file.each do |line|
@@ -37,19 +47,28 @@ loop {
       end
       status = "200"
       message = "OK"
+      socket.puts(version + " " + status + " " + message)
+      socket.puts("Date: " + Time.now.ctime)
     else
       status = "404"
       message = "Not Found"
+      socket.puts(version + " " + status + " " + message)
+      socket.puts("Date: " + Time.now.ctime)
     end
   else
     status = "400"
     message = "Bad Request"
+    socket.puts(version + " " + status + " " + message)
+    socket.puts("Date: " + Time.now.ctime)
   end
 
-  client.puts(version + " " + status + " " + message)
-  client.puts("Date: " + Time.now.ctime)
-  client.puts("Content-Length: " + body.length.to_s)
-  client.puts(body)
+  socket.print "#{version} #{status} #{message}\r\n" +
+               "Content-Type: #{content_type[:html]}\r\n" +
+               "Content-Length: #{body.bytesize}\r\n" +
+               "Connection: close\r\n"
+  socket.print "\r\n"
+  socket.print body
 
-  client.close
-}
+  socket.close
+
+end
