ceba6df5c824be9f738a1f8c6537ac429bff49c0
ThothLogos <naughton.joshua@gmail.com>
2015-02-18 21:34:06 -0500

    Correct JSON transmission, size computation

--- web_server/browser.rb
+++ web_server/browser.rb
@@ -7,41 +7,35 @@ port = 7680
 
 getpath = "/index.html"
 postpath = "/thanks.html"
-body = nil
+body = ""
 puts "Please enter 1) GET or 2) POST: "
 input = gets.chomp
 
 if input == "1" || input == "GET"
-  request = "GET #{getpath} HTTP/1.0\r\n\r\n"
+  request = "GET #{getpath} HTTP/1.1\r\n\r\n"
 elsif input == "2" || input == "POST"
   raid = Hash.new
   member = Hash.new
-  
-  puts "Enter a name: "
-  name = gets.chomp
+  name = "Gets Chaeump"
+  email = "email@emails.org"
   member[:name] = name
-  
-  puts "Enter email: "
-  email = gets.chomp
   member[:email] = email
-
   raid[:viking] = member
   body = raid.to_json
-
-  request = "POST #{postpath} HTTP/1.0\r\n\r\n"
-else
-  puts "Error."
+  request = "POST #{postpath} HTTP/1.1"
 end
 
 
-
+puts body
 
 socket = TCPSocket.open(host, port)
-socket.puts(request)
-if body != nil
-  socket.puts("Content-Length: " + body.length.to_s)
-  socket.puts(body)
-end
-response = socket.read
+socket.print "#{request}\r\n" +
+             "Content-Type: application/json\r\n" +
+             "Content-Length: #{body.bytesize}\r\n"
+socket.print "\r\n"
+socket.print body if body != ""
 
+
+
+response = socket.read
 print response
