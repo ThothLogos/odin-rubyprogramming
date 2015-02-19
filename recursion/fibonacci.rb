8beb3609d2a8df8ba5aa7c7fba0c98a19de24e79
ThothLogos <naughton.joshua@gmail.com>
2015-02-19 01:02:44 -0500

    Implement fib sequence using both iterative and recursive methods

--- recursion/fibonacci.rb
+++ recursion/fibonacci.rb
@@ -1,2 +1,32 @@
 # The Odin Porject - Section 3: Ruby Programming
 # Project File - Computer Science: No. 1, Recursion - Fibonacci
+
+
+# Implementation using iteration, prints the set of fibonacci numbers
+# up to the quantity passed into the function
+def fibs(n)
+  fibs = [] # Array to hold our sequence
+  i = 0; j = 1 # First two fibs
+  fibs << i; fibs << j
+  (n-1).times do
+    newfib = i + j
+    fibs << newfib # Add each fib to the result set
+    i = j
+    j = newfib
+  end
+  return fibs
+end
+
+# Implementation using recursion
+def fibs_rec(n, fibs = [0,1])
+  # Escape when countdown is at the bottom range
+  if n < 2
+    return fibs
+  else
+    # If not, add the new fib, recall self with n-1
+    fibs_rec(n-1, fibs << fibs[-1] + fibs[-2])
+  end
+end
+
+print fibs(12)
+print fibs_rec(12)
