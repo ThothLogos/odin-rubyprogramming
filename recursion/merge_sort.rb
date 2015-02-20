53d3ae45e27d8038dade84d7b151d8d2a3efec6c
ThothLogos <naughton.joshua@gmail.com>
2015-02-19 19:01:32 -0500

    Add merge_sort using recursion, test case

--- recursion/merge_sort.rb
+++ recursion/merge_sort.rb
@@ -1,2 +1,36 @@
 # The Odin Porject - Section 3: Ruby Programming
 # Project File - Computer Science: No. 1, Recursion - Merge Sort
+
+# Implementation of recursive merge sorting. There are 3 basic steps: divide
+# into smaller sets, recursively sort the smaller sets (dividing again), then
+# recombine using a merge comparison.
+
+def merge_sort(nums)
+  # Base case, if the recursion is being done on a single element, no sorting can be
+  # done and we can begin collapsing the recursion branch
+  return nums if nums.length <= 1
+  # Divide
+  a = nums[0..(nums.length/2)-1]
+  b = nums[(nums.length/2)..-1]
+  # Conquer
+  result = merge(merge_sort(a), merge_sort(b))
+end
+
+# Compares two incoming arrays, returns a single sorted array
+def merge(a, b)
+  merged = []
+  until a.empty? || b.empty?
+    # Grab the smaller of the two, move it into the result
+    if a.first <= b.first
+      merged << a.shift # Pops first element out of 
+    else
+      merged << b.shift
+    end
+  end
+  # Append final remaining element
+  merged += a += b
+end
+
+array = [2, 8, 9, 1, 4, 0, 10, 100, 53, 12]
+
+print merge_sort(array)
