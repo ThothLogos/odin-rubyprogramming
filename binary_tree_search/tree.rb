0f7e709b0ea5a0f9be132ea35b8f6dee7b55624b
ThothLogos <naughton.joshua@gmail.com>
2015-02-20 17:46:32 -0500

    Add basic Tree class structure

---
+++ binary_tree_search/tree.rb
@@ -0,0 +1,28 @@
+# The Odin Project - Section 3: Ruby Programming
+# Computer Science: Data Structures & Algorithms, No. 1 Searching Binary Trees
+
+class Tree
+
+  def initialize(numbers)
+    @numbers = numbers
+    @root = numbers[numbers.length/2]
+  end
+
+
+  def build_tree(@numbers, @root)
+    
+  end
+
+  def numbers() @numbers end
+  def root() @root end
+
+  def insert_node(node)
+
+  end
+
+  def remove_node(node)
+
+  end
+
+
+end
