30b127ada31b58702deb2565dc211a8c34c2cd1f
ThothLogos <naughton.joshua@gmail.com>
2015-02-20 17:46:07 -0500

    Add basic Node class structure

--- binary_tree_search/node.rb
+++ binary_tree_search/node.rb
@@ -1,2 +1,27 @@
 # The Odin Project - Section 3: Ruby Programming
 # Computer Science: Data Structures & Algorithms, No. 1 Searching Binary Trees
+
+
+class Node
+
+  def initialize(value = nil, parent = nil, children = nil, tier = nil)
+    @value = value
+    @parent = parent
+    @children = children
+    @tier = tier
+  end
+
+
+
+  def value() @value end
+  def set_value(value) @value = value end
+  
+  def parent() @parent end
+  def set_parent(parent) @parent = parent end
+  
+  def children() @children end
+  def set_children(children) @children = children end
+
+  def tier() @tier end
+  def set_tier(tier) @tier = tier end
+end
