24fa5ae1ebcad8ee160dfdd2d3ce5bce660008b7
ThothLogos <naughton.joshua@gmail.com>
2015-02-21 02:11:55 -0500

    Debug messages

--- binary_tree_search/tree.rb
+++ binary_tree_search/tree.rb
@@ -12,28 +12,38 @@ class Tree
   end
 
   def build_tree(arr, parent)
+    
     midpoint = arr[arr.length/2]
     node = Node.new(midpoint, parent)
+    if arr.length == 1
+      print "_LEAF_ Node: #{node.key}, parent: #{parent}\n"
+      return
+    end
 
-    return if arr.length == 1
+    print "BRANCH Node: #{node.key}, parent: #{parent}\n"
 
     left_arr = arr[0..(arr.length/2)-1]
     right_arr = arr[(arr.length/2)+1..-1]
     
-    unless left_arr.nil? || left_arr.empty?
+    unless left_arr.empty?
       node.set_left_child(left_arr)
+      print "update Node: #{node.key}, Left add: #{node.left_child}\n"
       build_tree(left_arr, node.key)
     end
 
-    unless right_arr.nil? || right_arr.empty?
+    unless right_arr.empty?
       node.set_right_child(right_arr)
+      print "update Node: #{node.key}, Right add: #{node.right_child}\n"
       build_tree(right_arr, node.key)
     end
+
+    print "Branch Complete: #{node.key}, P: #{parent}, L: #{node.left_child}, R: #{node.right_child}\n"
   end
 
 
   def view_tree
-    
+
+  end
 
   def insert_node(node)
   end
@@ -47,7 +57,11 @@ class Tree
 end
 
 
-small = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+small = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
 primes = [2, 3, 5, 7, 11, 13, 17, 19, 23, 31, 37, 41,
          43, 47, 53, 59, 61, 67, 71, 79, 83, 89, 97]
+
+primes_small = [2, 3, 5, 7, 11, 13, 17, 19, 23, 31, 37, 41, 43, 47, 53]
+
+tree = Tree.new(primes_small)
