c575d804d64e825998a0f4bd2cc11d02139d4218
ThothLogos <naughton.joshua@gmail.com>
2015-02-21 02:27:14 -0500

    Debug messages

--- binary_tree_search/tree.rb
+++ binary_tree_search/tree.rb
@@ -12,37 +12,30 @@ class Tree
   end
 
   def build_tree(arr, parent)
-    
     midpoint = arr[arr.length/2]
     node = Node.new(midpoint, parent)
     if arr.length == 1
-      print "_LEAF_ Node: #{node.key}, parent: #{parent}\n"
+      puts "  LEAF\t\t\t Node: #{node.key}\t Parent: #{parent}"
       return
     end
-
-    print "BRANCH Node: #{node.key}, parent: #{parent}\n"
-
+    puts "NEW BRANCH NODE\t\t Node: #{node.key}\t Parent: #{parent}"
     left_arr = arr[0..(arr.length/2)-1]
     right_arr = arr[(arr.length/2)+1..-1]
-    
+
     unless left_arr.empty?
       node.set_left_child(left_arr)
-      print "update Node: #{node.key}, Left add: #{node.left_child}\n"
+      puts "      update\t\t Node: #{node.key}\t LChild: #{node.left_child}"
       build_tree(left_arr, node.key)
     end
-
     unless right_arr.empty?
       node.set_right_child(right_arr)
-      print "update Node: #{node.key}, Right add: #{node.right_child}\n"
+      puts "      update\t\t Node: #{node.key}\t RChild: #{node.right_child}"
       build_tree(right_arr, node.key)
     end
-
-    print "Branch Complete: #{node.key}, P: #{parent}, L: #{node.left_child}, R: #{node.right_child}\n"
+    puts "Branch Complete\t\t Node: #{node.key}\t Parent: #{parent}\t Left: #{node.left_child}\t Right: #{node.right_child}"
   end
 
-
   def view_tree
-
   end
 
   def insert_node(node)
@@ -53,9 +46,7 @@ class Tree
 
   def numbers() @numbers end
   def root() @root end
-
 end
-
 
 small = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
